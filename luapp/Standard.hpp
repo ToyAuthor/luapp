c658b17443e3340faa445e11f5fab0d626d9b594
ToyAuthor <eye5002003@gmail.com>
2016-11-18 18:39:16 +0800

    Add my std::shared_ptr.

--- luapp/Standard.hpp
+++ luapp/Standard.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include "luapp/stl/memory.hpp"
 #include "luapp/LuaAPI.hpp"
 #include "luapp/PushToStack.hpp"
 #include "luapp/CopyFromStack.hpp"
---
+++ luapp/stl/memory.hpp
@@ -0,0 +1,8 @@
+
+#include "luapp/Config.hpp"
+
+#ifdef _LUAPP_CPP11_
+	#include <memory>
+#else
+	#include "luapp/stl/sharedptr.hpp"
+#endif
